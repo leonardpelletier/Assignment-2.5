6761d4af9cfc2fe9eb33e64699088887b5ee995a
leonardpelletier <51107522+leonardpelletier@users.noreply.github.com>
2019-10-09 14:48:17 -0700

    Add files via upload

--- Exercise6.java
+++ Exercise6.java
@@ -5,8 +5,8 @@ class Exercise6 {
     }
     
     public static void displayBox(int width, int height) {
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
                 System.out.print("$");
             }
             System.out.println("");
